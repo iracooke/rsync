e3601b99b22528e4d781084e0cfd7316de4f87f1
Ira Cooke <iracooke@iras-imac.bigpond>
2011-10-30 14:16:09 +1100

    Initial Commit

---
+++ rsync/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  rsync
+//
+//  Created by Ira Cooke on 30/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
